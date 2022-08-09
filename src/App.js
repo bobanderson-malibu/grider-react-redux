c47180345277673490522ccb4ab9950c09c910f2
bobanderson-malibu <bobanderson.malibu@gmail.com>
2022-08-09 21:25:30 +0000

    @faker-js/faker updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import {faker} from '@faker-js/faker';
  
 const App = () => {
   return (
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" />
+          <img alt="avatar" src={faker.image.avatar()} />
         </a>
         <div className="content">
           <a href="/" className="author">
