9a9ca59f70851e334c291908745716364f783ed5
bobanderson-malibu <bobanderson.malibu@gmail.com>
2022-08-10 19:03:03 +0000

    a fresh start

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-//import logo from './logo.svg';
 import './App.css';
-import CommentDetail from './CommentDetail';
-import {faker} from '@faker-js/faker';
-import ApprovalCard from './ApprovalCard';
+//import logo from './logo.svg';
+//import {faker} from '@faker-js/faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail 
-        author="Sam" 
-        timeAgo="Today at 4:45PM" 
-        avatar={faker.image.avatar()}
-        content="Awesome post, bruh!"
-        ></CommentDetail>
-      <CommentDetail 
-        author="Alex" 
-        timeAgo="Today at 2:00AM"
-        avatar={faker.image.avatar()}
-        content="Kool post, dood!"
-        ></CommentDetail>
-      <CommentDetail 
-        author="Jane" 
-        timeAgo="Yesterday at 5:00PM"
-        avatar={faker.image.avatar()}
-        content="Lovely article, sista!"
-        ></CommentDetail>
     </div>
   );
 }
