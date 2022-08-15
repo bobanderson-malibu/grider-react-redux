92f81b27e839297eccb9bb6c465a0b401afcdb94
bobanderson-malibu <bobanderson.malibu@gmail.com>
2022-08-15 22:55:00 +0000

    react hooks chapter till ch-60 xss wikipedia results -- completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,28 @@
-import React from 'react';
-import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
-import VideoList from './VideoList';
-import VideoDetail from './VideoDetail';
+import Accordion from "./Accordion";
+import Search from "./Search";
 
-class App extends React.Component {
-    state = { videos: [], selectedVideo: null };
-
-    componentDidMount () {
-        this.onTermSubmit('malibu surfing');
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a frontend javascript framework'
+    },
+    {   
+        title: 'Why use React?',
+        content: 'React is a favorite JS library among engineers'
+    },
+    {
+        title: 'How do use React?',
+        content: 'You use React by creating components'
     }
-    
-    onTermSubmit = async term => {
-        const response =  await youtube.get('/search',{
-            params: {
-                q: term
-            } 
-        });
-        this.setState({
-            videos: response.data.items,
-            selectedVideo: response.data.items[0]
-        });
-    };
+];
 
-    onVideoSelect = video => {
-        this.setState({selectedVideo: video}); 
-    };
-    
-    render() {
-       return (
-        <div className="ui container">
-            <SearchBar onFormSubmit={this.onTermSubmit}  />
-            <div className="ui grid">
-                <div className="ui row">
-                    <div className="eleven wide column">
-                      <VideoDetail video={this.state.selectedVideo}/>  
-                    </div>
-                    <div className="five wide column">
-                       <VideoList onVideoSelect={this.onVideoSelect}  videos={this.state.videos}/>  
-                    </div>
-                </div>
-            </div>
-        </div>  
-     ); 
-    }   
-}
+const App = () => {
+    return (
+        <div>
+            {/* <Accordion items={items}/> */}
+            <Search />
+        </div>
+    );
+};
 
 export default App;
