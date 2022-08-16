1663a0e58cd699d8eb92d047dbf2dab84c8a1fbf
Bob Anderson <bobanderson.malibu@gmail.com>
2022-08-16 13:58:44 -0700

    ch 186 Body Event Listener cleanup - complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import Accordion from "./Accordion";
 import Search from "./Search";
+import Dropdown from "./Dropdown";
 
 const items = [
     {
@@ -16,11 +18,33 @@ const items = [
     }
 ];
 
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'A Shade of blue',
+        value: 'blue'
+    }
+];
+
 const App = () => {
+    const [selected, setSelected] = useState(options[0]);
+    
     return (
         <div>
             {/* <Accordion items={items}/> */}
-            <Search />
+            {/* <Search /> */}
+            <Dropdown
+                selected={selected}
+                onSelectedChange={setSelected} 
+                options={options}
+            />
         </div>
     );
 };
--- src/components/Search.js
+++ src/components/Search.js
@@ -3,8 +3,19 @@ import axios  from "axios";
 import { queryAllByAltText } from "@testing-library/react";
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [debouncedTerm, setdebouncedTerm] = useState(term);
     const [results, setResults] = useState([]); 
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setdebouncedTerm(term);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    },[term]);
 
     useEffect(() => {
         const search = async () => {
@@ -14,19 +25,55 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term 
+                    srsearch: debouncedTerm 
                 }
             });
             setResults(data.query.search); 
         };
-        if (term) {
-          search();  
-        }
-    },[term]);
+
+        search();
+
+    },[debouncedTerm]);
+
+    // useEffect(() => {
+    //     const search = async () => {
+    //         const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+    //             params :{
+    //                 action: 'query',
+    //                 list: 'search',
+    //                 origin: '*',
+    //                 format: 'json',
+    //                 srsearch: term 
+    //             }
+    //         });
+    //         setResults(data.query.search); 
+    //     };
+
+    //     if (term && !results.length) {
+    //         search();
+    //     } else {
+    //         const timeoutId = setTimeout(() => {
+    //             if (term) {
+    //                 search();  
+    //             }
+    //         }, 1000);
+
+    //         return () => {
+    //             clearTimeout(timeoutId);
+    //         }; 
+    //     }
+        
+    // },[term]);
  
     const renderedResults = results.map((result) => {
         return (
             <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a 
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >Go</a>
+                </div>
                 <div className="content">
                      <div className="header">
                          {result.title}
